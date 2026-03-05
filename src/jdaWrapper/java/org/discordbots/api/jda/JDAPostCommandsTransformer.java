77200fd46b4486b505e5de1431d6146e6826090e
null <60427892+null8626@users.noreply.github.com>
2026-03-05 18:03:14 +0700

    feat: add JDA helpers

---
+++ src/jdaWrapper/java/org/discordbots/api/jda/JDAPostCommandsTransformer.java
@@ -0,0 +1,34 @@
+package org.discordbots.api.jda;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonParser;
+import java.util.concurrent.CompletionStage;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.interactions.commands.Command;
+import net.dv8tion.jda.api.interactions.commands.build.CommandData;
+import org.discordbots.api.io.PostCommandsTransformer;
+
+public class JDAPostCommandsTransformer implements PostCommandsTransformer {
+  private final JDA jda;
+
+  public JDAPostCommandsTransformer(final JDA jda) {
+    this.jda = jda;
+  }
+
+  @Override
+  public CompletionStage<String> toJsonString() {
+    return jda.retrieveCommands()
+        .submit()
+        .thenApply(
+            commands -> {
+              final JsonArray object = new JsonArray();
+
+              for (final Command command : commands) {
+                object.add(
+                    JsonParser.parseString(CommandData.fromCommand(command).toData().toString()));
+              }
+
+              return object.toString();
+            });
+  }
+}
