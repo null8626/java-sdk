c505c0a033cc311accbb69055e31c8dcd8279259
null <60427892+null8626@users.noreply.github.com>
2026-03-05 18:02:38 +0700

    feat: add discord4j helpers

---
+++ src/discord4jWrapper/java/org/discordbots/api/discord4j/Discord4JPostCommandsTransformer.java
@@ -0,0 +1,37 @@
+package org.discordbots.api.discord4j;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import discord4j.common.JacksonResources;
+import discord4j.core.DiscordClient;
+import java.util.concurrent.CompletionStage;
+import org.discordbots.api.io.PostCommandsTransformer;
+
+public class Discord4JPostCommandsTransformer implements PostCommandsTransformer {
+  private final DiscordClient client;
+
+  public Discord4JPostCommandsTransformer(final DiscordClient client) {
+    this.client = client;
+  }
+
+  @Override
+  public CompletionStage<String> toJsonString() {
+    return this.client
+        .getApplicationId()
+        .toFuture()
+        .thenCompose(
+            applicationId ->
+                this.client
+                    .getApplicationService()
+                    .getGlobalApplicationCommands(applicationId)
+                    .collectList()
+                    .toFuture())
+        .thenApply(
+            commands -> {
+              try {
+                return JacksonResources.create().getObjectMapper().writeValueAsString(commands);
+              } catch (final JsonProcessingException ignored) {
+                return "[]";
+              }
+            });
+  }
+}
