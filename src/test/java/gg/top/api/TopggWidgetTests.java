b7b4ab955c2184df39df9b8657a3571a26f70c4a
null <60427892+null8626@users.noreply.github.com>
2026-03-11 20:37:59 +0700

    fix: reflect new changes on widget throughout tests

--- src/test/java/gg/top/api/TopggWidgetTests.java
+++ src/test/java/gg/top/api/TopggWidgetTests.java
@@ -1,31 +1,49 @@
 package gg.top.api;
 
+import java.util.stream.Stream;
+
+import gg.top.api.entity.Platform;
 import gg.top.api.entity.ProjectType;
+
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.EnumSource;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 public class TopggWidgetTests {
-  @ParameterizedTest
-  @EnumSource(ProjectType.class)
-  public void large(final ProjectType projectType) {
-    TopggWidget.large(projectType, "123456");
+  @SuppressWarnings("unused")
+  static Stream<Arguments> platformsAndProjectTypes() {
+    final Stream.Builder<Arguments> streamBuilder = Stream.builder();
+
+    for (final Platform platform : Platform.values()) {
+      for (final ProjectType projectType : ProjectType.values()) {
+        streamBuilder.add(Arguments.of(platform, projectType));
+      }
+    }
+
+    return streamBuilder.build();
   }
 
   @ParameterizedTest
-  @EnumSource(ProjectType.class)
-  public void votes(final ProjectType projectType) {
-    TopggWidget.votes(projectType, "123456");
+  @MethodSource("platformsAndProjectTypes")
+  public void large(final Platform platform, final ProjectType projectType) {
+    TopggWidget.large(platform, projectType, "123456");
   }
 
   @ParameterizedTest
-  @EnumSource(ProjectType.class)
-  public void owner(final ProjectType projectType) {
-    TopggWidget.owner(projectType, "123456");
+  @MethodSource("platformsAndProjectTypes")
+  public void votes(final Platform platform, final ProjectType projectType) {
+    TopggWidget.votes(platform, projectType, "123456");
   }
 
   @ParameterizedTest
-  @EnumSource(ProjectType.class)
-  public void social(final ProjectType projectType) {
-    TopggWidget.social(projectType, "123456");
+  @MethodSource("platformsAndProjectTypes")
+  public void owner(final Platform platform, final ProjectType projectType) {
+    TopggWidget.owner(platform, projectType, "123456");
+  }
+
+  @ParameterizedTest
+  @MethodSource("platformsAndProjectTypes")
+  public void social(final Platform platform, final ProjectType projectType) {
+    TopggWidget.social(platform, projectType, "123456");
   }
 }
